bc4f99a27b65368d5d748f9d464945d860cbfb59
oskhen <oskar.henriksson2001@gmail.com>
2020-12-12 18:44:10 +0100

    Added config.cfg and parser

---
+++ parseconf.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import arcade
+from pymunk.vec2d import Vec2d
+from logic import Player
+
+def parsefile(filename):
+
+    f = open(filename, "r").readlines()
+    conf = [i.strip("\n") for i in f if i[0] != "#" and i[0] != "\n"]
+    
+    movemapping = {
+        "Up" : Vec2d(0, 1),
+        "Down": Vec2d(0, -1),
+        "Left": Vec2d(-1, 0),
+        "Right": Vec2d(1, 0),
+    }
+
+    actionmapping = {
+        "Shoot" : Player.shoot,
+        "Dash" : Player.dash,
+    }
+
+    p1_move_keys = dict()
+    p2_move_keys = dict()
+    p1_action_keys = dict()
+    p2_action_keys = dict()
+    p1_name = ""
+    p2_name = ""
+    ai_settings = ""
+    p1_ai_path = ""
+    p2_ai_path = ""
+
+    for setting in conf:
+
+        settingmapping = setting.split(" = ")
+        
+        if "key" in settingmapping[0]:
+            keytype = settingmapping[0].split("_")[2]
+            maptype = eval(keytype + "mapping")
+            func = "arcade.key." + settingmapping[1]
+            mapping = maptype[settingmapping[0].split("_")[-1]]
+            playerdict = eval(settingmapping[0][0:2].lower() + "_" + keytype + "_keys")
+            playerdict[eval(func)] = mapping
+
+        elif "AI" in settingmapping[0]:
+            if settingmapping[0] == "AI":
+                ai_settings = settingmapping[1]
+            elif "P1" in settingmapping[0]:
+                p1_ai_path = settingmapping[1]
+            elif "P2" in settingmapping[0]:
+                p2_ai_path = settingmapping[1]
+
+        elif settingmapping[0] == "P1":
+            p1_name = settingmapping[1]
+        
+        elif settingmapping[0] == "P2":
+            p2_name = settingmapping[1]
+    
+    return [ai_settings, p1_name, p1_move_keys, p1_action_keys, p1_ai_path, p2_name, p2_move_keys, p2_action_keys, p2_ai_path]
+
+                
+
+
+if __name__ == "__main__":
+    x = parsefile("config.cfg")
+    for y in x:
+        print(y)
